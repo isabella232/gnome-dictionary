405c2bfd152c6cd90c0973d9501b51ade28b46e4
Emmanuele Bassi <ebassi@gnome.org>
2017-04-11 21:37:02 +0100

    Drop gtk-doc
    Nobody uses libgdict except us, and the API reference has been
    hopelessly out of date for years.

--- autogen.sh
+++ autogen.sh
@@ -13,18 +13,6 @@ rm -rf autom4te.cache
 
 mkdir -p m4
 
-GTKDOCIZE=$(which gtkdocize 2>/dev/null)
-if test -z $GTKDOCIZE; then
-        echo "*** You don't have gtk-doc installed, and thus won't be able to generate the documentation. ***"
-        rm -f gtk-doc.make
-        cat > gtk-doc.make <<EOF
-EXTRA_DIST =
-CLEANFILES =
-EOF
-else
-        gtkdocize || exit $?
-fi
-
 AUTORECONF=$(which autoreconf 2>/dev/null)
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
